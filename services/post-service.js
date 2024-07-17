bec82a70bd35042cd48fd11c1703990a2b2f9da2
문현민 <munhyeonmin@munhyeonmin-ui-MacBookPro.local>
2024-07-17 16:21:33 +0900

    리스트 페이지 추가

--- services/post-service.js
+++ services/post-service.js
@@ -1,3 +1,5 @@
+const paginator = require("../utils/paginator");
+
 // 글쓰기
 async function writePost(collection, post) {
   // 글쓰기 함수
@@ -7,6 +9,21 @@ async function writePost(collection, post) {
   return await collection.insertOne(post);
 }
 
+async function list(collection, page, search) {
+  const perPage = 10;
+  const query = { title: new RegExp(search, "i") }; // title이 search와 부분일치하는지 확인
+  const cursor = collection
+    .find(query, { limit: perPage, ski: (page - 1) * perPage })
+    .sort({
+      // limit은 10개만 가져온다는 의미, skip은 설정된 개수만큼 건너뛴다(skip)
+      createdDt: -1, // 생성일 역순으로 정리
+    });
+  const totalCount = await collection.count(query);
+  const posts = await cursor.toArray(); // 커서로 받아온 데이터를 리스트로 변경
+  const paginatorObj = paginator({ totalCount, page, perPage: perPage }); // 페이지네이터 생성
+}
+
 module.exports = {
+  list,
   writePost,
 };
